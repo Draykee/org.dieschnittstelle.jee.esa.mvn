2b0e51049834dd8d1e4b9d9bfc760a31ded0ae24
Kevin Mattutat <officialdrayke@outlook.de>
2018-10-04 14:39:31 +0200

    + solved task "BAS2 Reflection für Attribute und Methoden"

--- org.dieschnittstelle.jee.esa.bas/src/main/java/org/dieschnittstelle/jee/esa/basics/ShowAnnotations.java
+++ org.dieschnittstelle.jee.esa.bas/src/main/java/org/dieschnittstelle/jee/esa/basics/ShowAnnotations.java
@@ -4,33 +4,61 @@ package org.dieschnittstelle.jee.esa.basics;
 import org.dieschnittstelle.jee.esa.basics.annotations.AnnotatedStockItemBuilder;
 import org.dieschnittstelle.jee.esa.basics.annotations.StockItemProxyImpl;
 
-import static org.dieschnittstelle.jee.esa.utils.Utils.*;
+import java.util.Arrays;
 
-public class ShowAnnotations {
+import static org.dieschnittstelle.jee.esa.utils.Utils.show;
 
-	public static void main(String[] args) {
-		// we initialise the collection
-		StockItemCollection collection = new StockItemCollection(
-				"stockitems_annotations.xml", new AnnotatedStockItemBuilder());
-		// we load the contents into the collection
-		collection.load();
+public class ShowAnnotations
+{
 
-		for (IStockItem consumable : collection.getStockItems()) {
-			;
-			showAttributes(((StockItemProxyImpl)consumable).getProxiedObject());
-		}
+    public static void main( String[] args )
+    {
+        // we initialise the collection
+        StockItemCollection collection = new StockItemCollection(
+                "stockitems_annotations.xml", new AnnotatedStockItemBuilder() );
+        // we load the contents into the collection
+        collection.load();
 
-		// we initialise a consumer
-		Consumer consumer = new Consumer();
-		// ... and let them consume
-		consumer.doShopping(collection.getStockItems());
-	}
+        for ( IStockItem consumable : collection.getStockItems() )
+        {
+            showAttributes( ( (StockItemProxyImpl) consumable ).getProxiedObject() );
+        }
 
-	/*
-	 * UE BAS2 
-	 */
-	private static void showAttributes(Object consumable) {
-		show("class is: " + consumable.getClass());
-	}
+        // we initialise a consumer
+        Consumer consumer = new Consumer();
+        // ... and let them consume
+        consumer.doShopping( collection.getStockItems() );
+    }
+
+    /*
+     * UE BAS2
+     */
+    private static void showAttributes( Object consumable )
+    {
+
+        show( "class is: " + consumable.getClass() );
+
+        StringBuilder fieldStr = new StringBuilder();
+        //Add classname
+        fieldStr.append( consumable.getClass().getSimpleName() );
+
+        //Add field names
+        Arrays.asList( consumable.getClass().getDeclaredFields() ).forEach( field -> {
+            try
+            {
+                //Override accessibility of the field to get its value
+                field.setAccessible( true );
+                fieldStr.append( String.format( " ,%s:%s", field.getName(), field.get( consumable ) ) );
+                //Make it "private" again
+                field.setAccessible( false );
+            }
+            catch ( IllegalAccessException e )
+            {
+                e.printStackTrace();
+            }
+        } );
+
+        show( String.format( "{%s}", fieldStr.toString() ) );
+    }
 
 }
